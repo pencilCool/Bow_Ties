18dc798a86f5f6b1a6541812f688f9a14e1ae5b8
pencilCool <yhtangcoder@gmail.com>
2017-09-13 10:22:46 +0800

    include test in appdelegate

---
+++ Bowtie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Bowtie+CoreDataClass.swift
+//  Bow Ties
+//
+//  Created by pencilCool on 2017/9/13.
+//  Copyright © 2017年 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Bowtie)
+public class Bowtie: NSManagedObject {
+
+}
